17efd249581b15ab230c6e43b7132d4ac0d7291b
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-09-18 07:49:54 -0400

    implimenting prepare for segue

--- Vocab/Vocab/C/WordsTableViewController.swift
+++ Vocab/Vocab/C/WordsTableViewController.swift
@@ -50,7 +50,12 @@ class WordsTableViewController: UITableViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDefineSegue" {
+            guard let detailedVC = segue.destination as? DefinitionViewController else { return }
+        }
+        
         // Get the new view controller using segue.destination.
+        
         // Pass the selected object to the new view controller.
     }
     
