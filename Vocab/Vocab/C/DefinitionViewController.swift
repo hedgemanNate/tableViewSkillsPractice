3442295f9c35303c6502ad130a6f59025ee503f7
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-09-18 07:18:04 -0400

    adds view controller files for the table view and the detailed definitions view

---
+++ Vocab/Vocab/C/DefinitionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DefinitionViewController.swift
+//  Vocab
+//
+//  Created by Nathan Hedgeman on 9/18/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
